470da651e0c7afb9ef272c1fe4c9cde53fe39352
Sujith Sizon <sujithsizon@gmail.com>
2017-07-01 22:14:23 +0400

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-cd src
-git clone https://github.com/ros-perception/image_common.git
-git clone https://github.com/ros-drivers/usb_cam.git --branch develop
-git clone https://github.com/ros-perception/image_pipeline.git --branch indigo
-cd ..
+cd odroid_ws
+git submodule update --init --recursive
 chmod +x src/missionpkg/src/el_det_fi.py
 catkin_make
 source devel/setup.bash
