4f1233312bd41226428fc9ded03842587fce9c41
Sujith Sizon <sujithsizon@gmail.com>
2017-07-01 21:29:17 +0400

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -5,6 +5,7 @@ git clone https://github.com/ros-perception/image_common.git
 git clone https://github.com/ros-drivers/usb_cam.git --branch develop
 git clone https://github.com/ros-perception/image_pipeline.git --branch indigo
 cd ..
+chmod +x src/missionpkg/src/el_det_fi.py
 catkin_make
 source devel/setup.bash
 roslaunch missionpkg missionpkg.launch
