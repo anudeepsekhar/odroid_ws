cd80caf46fd4df8b57eff3e9e35942e3153a47ef
Saptadeep Debnath <saptadeep.deb@gmail.com>
2017-07-03 17:17:19 +0400

    two video cams included

--- src/missionpkg/src/video.cpp
+++ src/missionpkg/src/video.cpp
@@ -6,29 +6,56 @@
 #include <opencv2/highgui/highgui.hpp>
 
 static const std::string OPENCV_WINDOW = "Image window";
+static const std::string OPENCV_WINDOW1 = "Image window1";
 
 class ImageConverter
 {
   ros::NodeHandle nh_;
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
+  image_transport::Subscriber image_sub1_;
+//  image_transport::Publisher image_pub_;
+//  image_transport::Publisher image_pub1_;
 
 public:
   ImageConverter()
     : it_(nh_)
   {
     // Subscribe to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/camera/image_raw", 1,
+    image_sub_ = it_.subscribe("/usb_cam/image_raw", 1,
       &ImageConverter::imageCb, this);
-
+    image_sub1_ = it_.subscribe("/usb_cam1/image_raw", 1,
+      &ImageConverter::imageCb1, this);
 
     cv::namedWindow(OPENCV_WINDOW);
+    cv::namedWindow(OPENCV_WINDOW1);
   }
 
   ~ImageConverter()
   {
     cv::destroyWindow(OPENCV_WINDOW);
+    cv::destroyWindow(OPENCV_WINDOW1);
+  }
+
+  void imageCb1(const sensor_msgs::ImageConstPtr& msg)
+  {
+    cv_bridge::CvImagePtr cv_ptr1;
+    try
+    {
+      cv_ptr1 = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    // Update GUI Window
+    cv::imshow(OPENCV_WINDOW1, cv_ptr1->image);
+    cv::waitKey(3);
+
+    // Output modified video stream
+//    image_pub1_.publish(cv_ptr1->toImageMsg());
   }
 
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
@@ -49,7 +76,7 @@ public:
     cv::waitKey(3);
 
     // Output modified video stream
-    image_pub_.publish(cv_ptr->toImageMsg());
+//    image_pub_.publish(cv_ptr->toImageMsg());
   }
 };
 
